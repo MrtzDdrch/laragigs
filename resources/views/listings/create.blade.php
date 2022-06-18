bdb6a586b3dfec6c10136c2376fd5b76ccf4230b
Moritz Diedrich <mrtz.ddrch@gmx.de>
2022-06-18 14:52:57 +0200

    making form fields remember old inputs

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -11,15 +11,12 @@
     <form method="POST" action="/listings">
         @csrf
         <div class="mb-6">
-            <label
-                for="company"
-                class="inline-block text-lg mb-2"
-                >Company Name</label
-            >
+            <label for="company" class="inline-block text-lg mb-2">Company Name</label>
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="company"
+                value="{{old('company')}}"
             />
             @error('company')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -27,14 +24,13 @@
         </div>
 
         <div class="mb-6">
-            <label for="title" class="inline-block text-lg mb-2"
-                >Job Title</label
-            >
+            <label for="title" class="inline-block text-lg mb-2">Job Title</label>
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="title"
                 placeholder="Example: Senior Laravel Developer"
+                value="{{old('title')}}"
             />
             @error('title')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -42,16 +38,13 @@
         </div>
 
         <div class="mb-6">
-            <label
-                for="location"
-                class="inline-block text-lg mb-2"
-                >Job Location</label
-            >
+            <label for="location" class="inline-block text-lg mb-2">Job Location</label>
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="location"
                 placeholder="Example: Remote, Boston MA, etc"
+                value="{{old('location')}}"
             />
             @error('location')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -59,13 +52,12 @@
         </div>
 
         <div class="mb-6">
-            <label for="email" class="inline-block text-lg mb-2"
-                >Contact Email</label
-            >
+            <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="email"
+                value="{{old('email')}}"
             />
             @error('email')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -73,16 +65,12 @@
         </div>
 
         <div class="mb-6">
-            <label
-                for="website"
-                class="inline-block text-lg mb-2"
-            >
-                Website/Application URL
-            </label>
+            <label for="website" class="inline-block text-lg mb-2">Website/Application URL</label>
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="website"
+                value="{{old('website')}}"
             />
             @error('website')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -90,14 +78,13 @@
         </div>
 
         <div class="mb-6">
-            <label for="tags" class="inline-block text-lg mb-2">
-                Tags (Comma Separated)
-            </label>
+            <label for="tags" class="inline-block text-lg mb-2">Tags (Comma Separated)</label>
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="tags"
                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                value="{{old('tags')}}"
             />
         </div>
 
@@ -113,27 +100,20 @@
         </div> --}}
 
         <div class="mb-6">
-            <label
-                for="description"
-                class="inline-block text-lg mb-2"
-            >
-                Job Description
-            </label>
+            <label for="description" class="inline-block text-lg mb-2">Job Description</label>
             <textarea
                 class="border border-gray-200 rounded p-2 w-full"
                 name="description"
                 rows="10"
                 placeholder="Include tasks, requirements, salary, etc"
-            ></textarea>
+            >{{old('description')}}</textarea>
             @error('description')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
             @enderror
         </div>
 
         <div class="mb-6">
-            <button
-                class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
-            >
+            <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
                 Create Gig
             </button>
 
