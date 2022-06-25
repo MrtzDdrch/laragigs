4a4cdec332ba84f24971ad6db7f4879506166f56
Moritz Diedrich <mrtz.ddrch@gmx.de>
2022-06-25 20:33:45 +0200

    eloquent relationships between users and posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,14 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@doe.com',
 
-        Listing::factory(6)->create();
+        ]);
+
+        Listing::factory(6)->create(['user_id' => $user->id]);
 
         // Listing::create([
         //     'title' => 'Laravel Senior Developer', 
